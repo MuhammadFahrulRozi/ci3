b3b5293ffa6752ae452bf865f663ab32f337dadb
DESKTOP-EFHE4FP\Fahrossi <mfahrul530@gmail.com>
2018-03-28 13:11:04 +0700

    TUGAS01-17

---
+++ application/views/madura.php
@@ -0,0 +1,12 @@
+<html>
+<title>Blitar Is My Life</title>
+<body>
+	<h1>
+		LONG Live Blitar 
+	</h1>
+	<p>
+		Blitar Kota syarat akan budaya, kata orang blitar kota para pemimpin.
+		Dari presiden pertama Indonesia yaitu Ir.Soekarno dll.
+	</p>
+</body>
+</html>
